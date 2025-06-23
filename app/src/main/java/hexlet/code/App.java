345886e72009d701ff63ac85dca84cbf0d28b4e9
Anatolii Siuniaev <a.a.syunyaev@gmail.com>
2025-06-23 17:09:23 +0300

    Added basic cli app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,27 @@
 package hexlet.code;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+    private String help;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+    private String version;
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
