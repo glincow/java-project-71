f8317f39a3effa1c0139888c35e298322544cec2
Anatolii Siuniaev <a.a.syunyaev@gmail.com>
2025-06-23 17:26:31 +0300

    Added basic cli app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,19 +1,22 @@
 package hexlet.code;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
 import java.util.concurrent.Callable;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", version = "gendiff 1.0",
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
-    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
-    private String help;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
 
-    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
-    private String version;
+    @Parameters(description = "path to first file")
+    private String filepath1;
+    @Parameters(description = " path to second file")
+    private String filepath2;
 
     @Override
     public Integer call() throws Exception {
