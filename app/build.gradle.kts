4e7aa8ed5883c02c2b61a88e8c15a3a776ef04d7
Anatolii Siuniaev <a.a.syunyaev@gmail.com>
2025-07-28 20:17:00 +0300

    Added tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     id("application")
+    id("jacoco")
     id("checkstyle")
     id("org.sonarqube") version "6.2.0.5505"
 }
