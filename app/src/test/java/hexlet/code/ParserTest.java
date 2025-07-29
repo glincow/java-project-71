bc03900d33421bd50a316d367359bc719ecaedef
Anatolii Siuniaev <a.a.syunyaev@gmail.com>
2025-07-29 11:32:14 +0300

    Added parser tests

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest {
 
