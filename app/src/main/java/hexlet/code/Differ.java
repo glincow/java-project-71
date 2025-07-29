ccf545164dd6c5c5478143f82f3bc94c45f1f4f8
Anatolii Siuniaev <a.a.syunyaev@gmail.com>
2025-07-29 13:56:16 +0300

    Refactorings 3

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -12,4 +12,12 @@ public class Differ {
         List<DiffEntry> differences = DiffCalculator.calculate(data1, data2);
         return Formatter.format(differences, formatName);
     }
+
+    public static String generate(String filepath1,
+                                  String filepath2) throws Exception {
+        Map<String, Object> data1 = Parser.getData(filepath1);
+        Map<String, Object> data2 = Parser.getData(filepath2);
+        List<DiffEntry> differences = DiffCalculator.calculate(data1, data2);
+        return Formatter.format(differences, "stylish");
+    }
 }
